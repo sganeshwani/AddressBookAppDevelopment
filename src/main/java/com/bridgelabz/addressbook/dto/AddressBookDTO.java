cbc4f91331908b7b5f590e1d200c6c046d652a74
sganeshwani <ganeshwani2003@gmail.com>
2025-03-01 15:58:34 +0530

    [Sahil Ganeshwani] Add lombok library to auto generate getters setters for dto

--- src/main/java/com/bridgelabz/addressbook/dto/AddressBookDTO.java
+++ src/main/java/com/bridgelabz/addressbook/dto/AddressBookDTO.java
@@ -1,28 +1,14 @@
 package com.bridgelabz.addressbook.dto;
 
+import lombok.Data;
+
+@Data  // Lombok will auto-generate Getters, Setters, toString, Equals, and HashCode
 public class AddressBookDTO {
     private String name;
     private String address;
 
     public AddressBookDTO(String name, String address) {
         this.name = name;
-        this.address = address;
-    }
-
-    // Getters and Setters
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
         this.address = address;
     }
 }
